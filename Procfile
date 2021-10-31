9a3a3b89c5f1a6301fe149ec93fe6c7cec38be05
staffordcian99 <staffordcian99@gmail.com>
2021-10-31 08:39:07 +0000

    add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn brainfood.wsgi:application 
--- brainfood/settings.py
+++ brainfood/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-a*vm0em&^%yw^ekpq7xgmwslmwyslp1j9j7*1bnz(*u1w$g@@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['brainfood-ta-app.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -96,12 +96,16 @@ WSGI_APPLICATION = 'brainfood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
