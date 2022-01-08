4dd47dbfc132695c9cdd18320c0011e3fac41720
staffordcian99 <staffordcian99@gmail.com>
2022-01-08 17:14:47 +0000

    Remove country field

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -71,7 +71,6 @@ class StripeWH_Handler:
                 profile.default_street_address1 = shipping_details.address.line1
                 profile.default_street_address2 = shipping_details.address.line2
                 profile.default_county = shipping_details.address.state
-                profile.default_country = shipping_details.address.country
                 profile.save()
 
         order_exists = False
@@ -87,7 +86,6 @@ class StripeWH_Handler:
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
-                    country__iexact=shipping_details.address.country,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
