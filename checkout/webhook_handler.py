8dc2f7aa2f404bf92c5b3c4bbf5a506dba316e13
staffordcian99 <staffordcian99@gmail.com>
2022-01-08 15:02:40 +0000

    fix country field on checkout form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -110,12 +110,12 @@ class StripeWH_Handler:
                     user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
                     postcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
-                    country=shipping_details.address.country,
                     original_bag=bag,
                     stripe_pid=pid,
                 )
